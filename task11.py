9ce79352a0632f39185566e07e89e0da4f5d5181
Akshyageetham <109145031+Akshyageetham@users.noreply.github.com>
2025-02-04 22:59:38 +0000

    Add files via upload

---
+++ task11.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
+
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://jqueryui.com/droppable/")
+driver.maximize_window()
+driver.switch_to.frame(driver.find_element(By.TAG_NAME, "iframe"))
+draggable = driver.find_element(By.ID, "draggable")
+droppable = driver.find_element(By.ID, "droppable")
+actions = ActionChains(driver)
+actions.drag_and_drop(draggable, droppable).perform()
+time.slepep(4)
+driver.quit()
